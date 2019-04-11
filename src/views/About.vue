91a1914ae3c2cd8841e2fbe516f57a25a32e7825
liweisheng <liwsh666@126.com>
2019-04-11 10:49:44 +0800

    返回按钮样式

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <slot name="leg">1111111111</slot>
+        <slot name="head">222222222</slot>
+        <slot name="ttt" a=2 d=3 :user="text1">默认的那个</slot>
+        <slot name="leg">1111111111</slot>
+        <h1 v-if="val==120">{{head}}</h1>
+    </div>
 </template>
+<script>
+
+export default {
+    name: 'about',
+    props: {
+        head: {
+            type: Number,
+            default: 0
+        },
+        val: {
+            default: 0
+        }
+    },
+    data(){
+        return {
+            text1: {
+                a: 99,
+                b: 66
+            }
+        }
+    },
+    methods: {
+        alert() {
+            alert(41)
+        }
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <about :val='val' :head="val" @click="alert">
+            <div slot="ttt" slot-scope="user1">
+                <div>替换了吗{{JSON.stringify(user1)}}</div>
+            </div>
+            <div slot="head">我来组成头部</div>
+            <div slot="leg">我来组成大腿</div>
+        </about>
+    </div>
 </template>
-
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import About from './About.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+    name: 'home',
+    components: {
+        HelloWorld, About
+    },
+    data() {
+        return {
+            text: 120,
+            val: 12
+        }
+    },
+    methods: {
+        alert: function() {
+            alert(1)
+        }
+    }
 }
 </script>
